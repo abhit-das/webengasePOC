bf514ca8eb41eb8461d7f867d39a8220e7d9ec34
Alok <alok.kumar@meddo.in>
2021-03-17 23:03:30 +0530

    Initial commit

---
+++ src/main/java/com/meddo/poc/WebengasePocApplication.java
@@ -0,0 +1,13 @@
+package com.meddo.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebengasePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebengasePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meddo/poc/WebengasePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meddo.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebengasePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
